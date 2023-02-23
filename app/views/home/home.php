d9667f4e60a2c79d616dbd49ec2f6c54c109cb63
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-02-23 12:23:17 +0200

    only ajax email validation left to do

--- app/views/home/home.php
+++ app/views/home/home.php
@@ -1,24 +1,5 @@
 <?php
+    // app/views/home/home.php
+?>
 
-// app/views/home/home.php
-
-if (!empty($_SESSION['user']))
-{
-    echo 'User authorized';
-    echo ' <br>';
-    print_r($_SESSION['user']);
-    echo ' <br>';
-    echo '<a href="/users/doLogout">Logout</a>';
-    echo ' <br>';
-
-    echo $_SESSION['notice_already_logined'];
-    unset($_SESSION['notice_already_logined']); // delete error message
-    echo ' <br>';
-}
-else
-{
-    echo 'You are no authorized';
-    echo ' <br>';
-}
-
-echo 'This is home page';
+This is home page
